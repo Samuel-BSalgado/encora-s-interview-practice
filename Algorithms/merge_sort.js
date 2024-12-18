d59fe8afe66db329b8a0951fa2174448ebf9d009
Samuel-BSalgado <samuelb0279@gmail.com>
2024-12-17 19:30:21 -0600

    finish merge_sort

---
+++ Algorithms/merge_sort.js
@@ -0,0 +1,36 @@
+//Divide y conquista (merge sort)
+//dividir: dividir el arreglo en mitades hasta llegar a tamaño 1
+//conquistar: combinar esas mitades ordenándolas al volver a crear un array con el orden especificado
+
+const arrayDesordnado = [100, 5, 26, 27, 30, 50, 7, 8, 9, 1];
+
+function mergeSort(array) {
+  if (array.length <= 1) { //Manejar el caso más simple, que el array length sea menor a 2
+    return array;
+  }
+
+  const mid = Math.floor(array.length/2);
+  const left = mergeSort(array.slice(0, mid));
+  const right = mergeSort(array.slice(mid));
+
+  return mergeSides(left, right);
+}
+
+function mergeSides(left, right) {
+  let orderedArray = [];
+  let i = 0;
+  let j = 0;
+
+  while (i<left.length && j<right.length) {
+    if (left[i] < right[j]) {
+      orderedArray.push(left[i]);
+      i++;
+    } else {
+      orderedArray.push(right[j]);
+      j++;
+    }
+  }
+  return orderedArray.concat(left.slice(i)).concat(right.slice(j));
+}
+
+console.log(mergeSort(arrayDesordnado));
